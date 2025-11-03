d389a59c8cbf268db95d14ea801d7a8effd69716
eslam55 <eslamjabr55@gmail.com>
2025-11-03 18:20:16 +0200

    update serviceStation

--- src/ServiceStation/ServiceStation.java
+++ src/ServiceStation/ServiceStation.java
@@ -1,5 +1,43 @@
+import java.util.*;
+
 package ServiceStation;
 
 public class ServiceStation {
-    
+
+    private static Queue<Car> waitingQueue;
+    private static Semaphore mutex;
+    private static Semaphore empty;
+    private static Semaphore full;
+
+    private static int waitingAreaSize;
+    private static int numberOfPumps;
+    private static int numberOfCars;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of pumps: ");
+        numberOfPumps = sc.nextInt();
+
+        System.out.print("Enter waiting area size: ");
+        waitingAreaSize = sc.nextInt();
+
+        System.out.print("Enter number of cars: ");
+        numberOfCars = sc.nextInt();
+
+        waitingQueue = new LinkedList<>();
+        mutex = new Semaphore(1);
+        empty = new Semaphore(waitingAreaSize);
+        full = new Semaphore(0);
+        
+        for (int i = 1; i <= numberOfPumps; i++) {
+            Pump pump = new Pump(i, waitingQueue, mutex, empty, full);
+            pump.start();
+        }
+        for (int i = 1; i <= numberOfCars; i++) {
+            Car car = new Car("C" + i, waitingQueue, mutex, empty, full);
+            car.start();
+
+
+        }
+    sc.close();
+    }
 }
